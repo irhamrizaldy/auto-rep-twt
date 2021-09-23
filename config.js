21591833c5e3179906b694638a93abaa90c11b03
irhamrizaldy <43570194+irhamrizaldy@users.noreply.github.com>
2021-09-23 13:53:57 +0700

    init

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'RvQVtnf2IOiOpJC1sRErHLkco',
+	consumer_secret: '8LbwTnshEjRQq0WToQxVCqUIx0Yh8wJUb5oNnKgJSuZeM34z7P',
+	access_token: '1439979991261802501-NGHrh9DxPYVq0UDfW50VB5VRoiEMX6',
+	access_token_secret: '4d4PFhmVeCJMKPv4ULJEqpi5dKzn6dpjFC1CSegus86cA'
+}
